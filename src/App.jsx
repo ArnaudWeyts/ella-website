dba3d596806b3d2aeb1a9c80bcd28f631a111101
Arnaud Weyts <arnaud.weyts@gmail.com>
2018-05-26 14:10:24 +0200

    implement structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
+
 import './App.css';
+
+import Header from './Header';
+import Contact from './Contact';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1 className="App-title">Welcome to React</h1>
-      </header>
+      <Header />
+      <video className="Video">
+        <source />
+        Your browser does not support the video tag.
+      </video>
+      <div className="Segment">Who we are</div>
+      <div className="Segment">Grow your business</div>
+      <div className="Segment">What we can do for you</div>
+      <div className="Segment">Pricing</div>
+      <div className="Segment">Map</div>
+      <Contact />
+      <footer className="Footer" />
     </div>
   );
 }
