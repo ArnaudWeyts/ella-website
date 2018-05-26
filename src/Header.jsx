66b170c6277917f913c7908798d24ae23931f9e8
Arnaud Weyts <arnaud.weyts@gmail.com>
2018-05-26 14:09:16 +0200

    make header component

---
+++ src/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="Header">
+      <h1 className="Header__title">Ella</h1>
+      <nav className="Header__menu">
+        <a href="#">Link 1</a>
+        <a href="#">Link 2</a>
+        <a href="#">Link 3</a>
+        <a href="#">Link 4</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
