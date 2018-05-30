e4a3816540046e9b878fabc5b036a5195462d2d8
Arnaud Weyts <arnaud.weyts@gmail.com>
2018-05-30 08:38:17 +0200

    convert header to styled component

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,26 +1,50 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import ellaLogo from './img/ella-logo.svg';
 
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable jsx-a11y/href-no-hash */
 
-function Header() {
+const Header = styled.header`
+  padding: 2em;
+  display: flex;
+  align-items: center;
+`;
+
+const HeaderMenu = styled.nav`
+  margin: 0 auto;
+`;
+
+const HeaderLogo = styled.img`
+  height: 2.5em;
+  margin-bottom: -0.75em;
+`;
+
+const NavLink = styled.a`
+  margin: 0 1em;
+  text-decoration: none;
+  color: #000;
+  font-weight: bold;
+  border-bottom: ${({ selected }) => (selected ? 'solid 2px #eb8018;' : 0)};
+`;
+
+function HeaderContainer() {
   return (
-    <header className="Header">
-      <nav className="Header__menu">
-        <a className="Header__menu__item--selected" href="#">
+    <Header>
+      <HeaderMenu>
+        <NavLink selected href="#">
           Home
-        </a>
-        <a href="#">About</a>
-        <a href="#">
-          <img className="Header__logo" src={ellaLogo} alt="Ella logo" />
-        </a>
-        <a href="#">Service</a>
-        <a href="#">Contact</a>
-      </nav>
-    </header>
+        </NavLink>
+        <NavLink href="#">About</NavLink>
+        <NavLink href="#">
+          <HeaderLogo src={ellaLogo} alt="Ella logo" />
+        </NavLink>
+        <NavLink href="#">Service</NavLink>
+        <NavLink href="#">Contact</NavLink>
+      </HeaderMenu>
+    </Header>
   );
 }
 
-export default Header;
+export default HeaderContainer;
