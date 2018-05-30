2cb4c01c97e6973443f869f3ac0b7d0e0eb1cf0d
Arnaud Weyts <arnaud.weyts@gmail.com>
2018-05-30 09:43:24 +0200

    improve open button for chat, add badge

--- src/Webchat.jsx
+++ src/Webchat.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
-const Webchat = styled.div`
+import woman from './img/woman.svg';
+
+const Container = styled.div`
   position: fixed;
   bottom: 1.25em;
   right: 1.25em;
@@ -28,6 +30,30 @@ const CloseButton = styled.button`
   right: 8px;
 `;
 
+const OpenButton = styled.button`
+  cursor: pointer;
+  height: 5em;
+  width: 5em;
+  border-radius: 50%;
+  border: 1px solid #eb8018;
+  background: ${() => `#fff url(${woman})`};
+  background-position: center;
+  background-size: cover;
+`;
+
+const Badge = styled.div`
+  height: 1.25em;
+  width: 1.25em;
+  top: -0.3em;
+  left: -0.3em;
+  border-radius: 50%;
+  background: #eb8018;
+  text-align: center;
+  line-height: 1.25em;
+  color: #fff;
+  position: absolute;
+`;
+
 class WebchatContainer extends Component {
   constructor(props, context) {
     super(props, context);
@@ -47,20 +73,19 @@ class WebchatContainer extends Component {
 
   render() {
     return (
-      <React.Fragment>
-        <Webchat>
-          <WebchatLook active={this.state.showChat}>
-            <Iframe
-              title="webchat"
-              src="https://webchat.botframework.com/embed/project-siba-faqbotservice?s=mO_IJV-qtHA.cwA.55M.1fgxugry7WA18aX_h7X4Pv1FDJj-TL3j4Y9LVA8350Y"
-            />
-            <CloseButton onClick={this.toggleChat}>Close</CloseButton>
-          </WebchatLook>
-          <WebchatLook active={!this.state.showChat}>
-            <button onClick={this.toggleChat}>Open chat</button>
-          </WebchatLook>
-        </Webchat>
-      </React.Fragment>
+      <Container>
+        <WebchatLook active={this.state.showChat}>
+          <Iframe
+            title="webchat"
+            src="https://webchat.botframework.com/embed/project-siba-faqbotservice?s=mO_IJV-qtHA.cwA.55M.1fgxugry7WA18aX_h7X4Pv1FDJj-TL3j4Y9LVA8350Y"
+          />
+          <CloseButton onClick={this.toggleChat}>Close</CloseButton>
+        </WebchatLook>
+        <WebchatLook active={!this.state.showChat}>
+          <Badge>1</Badge>
+          <OpenButton onClick={this.toggleChat} />
+        </WebchatLook>
+      </Container>
     );
   }
 }
