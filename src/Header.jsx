06cb0e856c3162c27013714a92ebec22a3b42c3f
Arnaud Weyts <arnaud.weyts@gmail.com>
2018-05-30 08:30:27 +0200

    adjust header

--- src/Header.jsx
+++ src/Header.jsx
@@ -8,13 +8,16 @@ import ellaLogo from './img/ella-logo.svg';
 function Header() {
   return (
     <header className="Header">
-      <img className="Header__logo" src={ellaLogo} alt="Ella logo" />
-      <h1 className="Header__title">Ella</h1>
       <nav className="Header__menu">
-        <a href="#">Link 1</a>
-        <a href="#">Link 2</a>
-        <a href="#">Link 3</a>
-        <a href="#">Link 4</a>
+        <a className="Header__menu__item--selected" href="#">
+          Home
+        </a>
+        <a href="#">About</a>
+        <a href="#">
+          <img className="Header__logo" src={ellaLogo} alt="Ella logo" />
+        </a>
+        <a href="#">Service</a>
+        <a href="#">Contact</a>
       </nav>
     </header>
   );
