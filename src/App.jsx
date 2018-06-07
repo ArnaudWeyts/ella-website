198514363c487a35ab041b1e037352094f1195ae
Arnaud Weyts <arnaud.weyts@gmail.com>
2018-06-07 10:46:30 +0200

    use react-router to show home & demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,16 @@
 import React from 'react';
-import styled from 'styled-components';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import Header from './Header';
-import BeforeElla from './BeforeElla';
-import Contact from './Contact';
-import Webchat from './Webchat';
+import Home from './Home';
+import Demo from './Demo';
 
-/* eslint-disable jsx-a11y/media-has-caption */
-
-const Video = styled.video`
-  width: 100%;
-  height: 31.25em;
-  background: lightgrey;
-`;
-
-const Segment = styled.div`
-  height: 15.625em;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-const Footer = styled.footer`
-  height: 6.25em;
-  background: darkgrey;
-`;
-
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Video>
-        <source />
-        Your browser does not support the video tag.
-      </Video>
-      <BeforeElla />
-      <Segment>Grow your business</Segment>
-      <Segment>What we can do for you</Segment>
-      <Segment>Pricing</Segment>
-      <Segment>Map</Segment>
-      <Contact />
-      <Footer className="Footer" />
-      <Webchat />
-    </div>
-  );
-}
+const App = () => (
+  <Router>
+    <React.Fragment>
+      <Route exact path="/" component={Home} />
+      <Route path="/demo" component={Demo} />
+    </React.Fragment>
+  </Router>
+);
 
 export default App;
