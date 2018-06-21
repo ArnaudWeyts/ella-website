88b8791c94231ed7a07a7695c4272e89a814490c
Arnaud Weyts <arnaud.weyts@gmail.com>
2018-06-21 11:02:24 +0200

    update component, change language on select

--- src/Home/LanguagePicker.jsx
+++ src/Home/LanguagePicker.jsx
@@ -7,16 +7,17 @@ import { Button } from './styles';
 const Popup = styled.div`
   position: absolute;
   opacity: ${props => (props.show ? 1 : 0)};
-  height: ${props => (props.show ? 'auto' : 0)};
   transition: all 0.5s ease-in-out;
-  padding: 0.5em 0;
-
+  padding: 0.5em 0.5em;
   background: #0a6b94;
-
   border-radius: 5px;
   margin-top: 15px;
   display: flex;
   flex-direction: column;
+  pointer-events: ${props => (props.show ? 'auto' : 'none')};
+  margin-left: -0.75em;
+  box-sizing: border-box;
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
 
   &:after {
     bottom: 100%;
@@ -37,32 +38,43 @@ const Popup = styled.div`
   }
 `;
 
+const languages = ['en', 'nl', 'fr', 'de'];
+
 class LanguagePicker extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
       showPopup: false,
+      selectedLanguage: 'en',
     };
+
+    this.selectLanguage = this.selectLanguage.bind(this);
+  }
+
+  selectLanguage(i18n, language) {
+    i18n.changeLanguage(language);
+    this.setState({ selectedLanguage: language, showPopup: false });
+  }
+
+  renderLanguages(i18n) {
+    return languages.map(language => (
+      <Button key={language} border onClick={() => this.selectLanguage(i18n, language)}>
+        {language}
+      </Button>
+    ));
   }
 
   render() {
-    const { showPopup } = this.state;
+    const { showPopup, selectedLanguage } = this.state;
     return (
       <I18n>
         {(t, { i18n }) => (
           <React.Fragment>
             <Button border onClick={() => this.setState({ showPopup: !showPopup })}>
-              EN
+              {selectedLanguage}
             </Button>
-            <Popup show={showPopup}>
-              <Button border onClick={() => i18n.changeLanguage('en')}>
-                en
-              </Button>
-              <Button border onClick={() => i18n.changeLanguage('nl')}>
-                nl
-              </Button>{' '}
-            </Popup>
+            <Popup show={showPopup}>{this.renderLanguages(i18n)}</Popup>
           </React.Fragment>
         )}
       </I18n>
